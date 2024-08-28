43c7639045b4170be9fe36abf27d7388ea9c185a
clarud <clarencejw@gmail.com>
2024-08-28 15:45:26 +0800

    Add method to allocate schools

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import json
 import math
+from collections import defaultdict
 
 # Returns the euclidean distance between 2 points
 def calculate_euclidean_distance(p1, p2):
@@ -12,10 +13,10 @@ def calculate_weightage(student, school):
     distance_score = 1 / (1 + distance)
 
     # Check for alumni
-    is_alumni = 1 if student.get['alumni'] == school.get['name']) else 0
+    is_alumni = 1 if student.get['alumni'] == school['name'] else 0
 
     # Check for volunteer
-    is_volunteer = 1 if student.get['volunteer'] == school.get['name'] else 0
+    is_volunteer = 1 if student.get['volunteer'] == school['name'] else 0
 
     score = 50 * distance_score + 30 * is_alumni + 20 * is_volunteer
     return score
@@ -24,16 +25,27 @@ def calculate_weightage(student, school):
 def allocate_students(input_data):
     school_allocation = defaultdict(list)
 
+    school_capacity = {school['name']: school['maxAllocation'] for school in input_data['schools']}
+
     # Calculate weightage score for each student for each school
     weightage_score = [] # Store weightage score of each student in a list
     for student in input_data['students']:
         for school in input_data['schools']:
             score = calculate_weightage(student, school)
-            weightage_score.append((school['name'], student['id'], score)
+            weightage_score.append(school['name'], student['id'], score)
 
     # Sort weightage score
     weightage_score.sort(key = lambda x: (-x[2], x[1])) # Sort be descending score then ascending student id
 
+    # Allocate student to school
+    for school_name, student_id, score in weightage_score:
+        if school_capacity[school_name] > 0:
+            school_allocation[school_name].append(student_id)
+            school_capacity[school_name] -= 1
+
+    output_data = [{school: students} for school, students in school_allocation.items()]
+
+    return output_data
 
 # Read the input JSON
 with open('input.json', 'r') as f:
